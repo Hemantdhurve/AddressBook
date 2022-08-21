ab3f51190cd7104c603ec8238432e54831e90016
Hemantdhurve <hemantdhurve007@gmail.com>
2022-08-21 15:30:44 +0530

    [Hemant] Added UC1 Creating Contact

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AddressBookSystem;
+using System;
 
 namespace AddressBooksystem
 {
@@ -7,6 +8,31 @@ namespace AddressBooksystem
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program");
+
+            bool check = true;
+
+            while (check)
+            {
+                Console.WriteLine("Please select the Options :\n" + 
+                    "1)Enter Details to add Contact in List\n");
+
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        
+                        AddressBook addobj=new AddressBook();
+                        addobj.createContact();
+                        break;
+
+                    default:
+                        Console.WriteLine("Please select proper option");
+                        break;
+                }
+                check = false;
+                break;
+            }
         }
     }
 
